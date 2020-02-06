4cbdaf394f838944c660d06fb9e5dce005193c2f
recinilt <recepyeni@yahoo.com>
2020-02-07 00:18:34 +0300

    regexp örneği eklendi

--- js/regexpmail.js
+++ js/regexpmail.js
@@ -4,15 +4,23 @@ $(document).ready(function() {
 
 
 
-var myexp = /([a-z0-9_-]*)@sivers.org/i;
-var str ="My email is derek@sivers.org but no spam, ok?";
+var myexp = /([a-z0-9_-]*)@sivers.org/igm;
+var str ="My email is derek@sivers.org but no spam, reci@sivers.org ok?";
 var matches = myexp.exec(str);
 var rez="the whole thing is " + matches[0] + " and username is " + matches[1];
 console.log(matches);
 console.log(rez);
 
-$("#mydiv").html(matches);
+$("#mydiv").html("<h2>" + matches+"</h2>");
 $("#mydiv2").html(rez);
+
+var matches2 = str.match(myexp);
+console.log(matches2);
+$("#mydiv3").html(matches2);
+
+
+
 
 
 });
+
